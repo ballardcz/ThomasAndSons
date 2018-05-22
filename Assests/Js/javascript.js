bee7f4ae2da60387f5f0711caabe077afd7c7dda
ballardcz <ballardcz@gmail.com>
2018-05-22 16:07:42 -0700

    thomasandsons.html --> index.html & trying to fix input fields

--- Assests/Js/javascript.js
+++ Assests/Js/javascript.js
@@ -1,42 +1,52 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // event.preventDefault()
   var formInputs = [];
 
-  var name1 = document.getElementById("validationTooltip01");
+  var name1 = document.getElementById("validationTooltip01").value;
   console.log(name1);
 
+  var name2 = $('#validationTooltip01').attr('id').value;
+  console.log(name2);
 
-var name2 = $('#validationTooltip01').attr('id');
-console.log(name2);
+  var number = 1;
+  console.log(number);
 
-var number = 1;
-console.log(number);
-
+  $('#validationTooltip01').on('input', function() {
+  console.log("thishit");
   if (isNaN(name1) && name1 > 0) {
     $("#valid-tooltip").text("Looks Good!");
   } else if (name1 && name1 > 0) {
     $("#valid-tooltip").text("Write your name!");
-  }
-  else { 
+  } else {
     $("#valid-tooltip").text("");
   }
 
-    // $("#validationTooltip01").on("submit", function() {
-    //   $("#myform").validate({
-    //     // initialize the plugin
-    //     rules: {
-    //       field1: {
-    //         required: true,
-    //         email: true
-    //       },
-    //       field2: {
-    //         required: true,
-    //         minlength: 5
-    //       }
-    //     }
-    //   });
-    //   alert("Form has been submitted");
-    // });
+  // $("#submit").on("click", function (event) {
+  //   event.preventDefault();
+  //   firstName: $("#validationTooltip01").val().trim(),
+  //     role: $("#role").val().trim(),
+  //     age: $("#age").val().trim(),
+  //     forcePoints: $("#force-points").val().trim()
+
+  // });
+
+  // $("#validationTooltip01").on("submit", function() {
+  //   $("#myform").validate({
+  //     // initialize the plugin
+  //     rules: {
+  //       field1: {
+  //         required: true,
+  //         email: true
+  //       },
+  //       field2: {
+  //         required: true,
+  //         minlength: 5
+  //       }
+  //     }
+  //   });
+  //   alert("Form has been submitted");
+  // });
 
   //})
+});
 });
