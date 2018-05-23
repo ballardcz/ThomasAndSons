7f942fa71b4c42992448009491604db8752f6d07
ballardcz <ballardcz@gmail.com>
2018-05-23 12:32:20 -0700

    update

--- Assests/Js/javascript.js
+++ Assests/Js/javascript.js
@@ -2,23 +2,23 @@ $(document).ready(function () {
   // event.preventDefault()
   var formInputs = [];
 
+$('#submit').on('click', function() {
+  console.log("thishit");
+
   var name1 = document.getElementById("validationTooltip01").value;
   console.log(name1);
 
-  var name2 = $('#validationTooltip01').attr('id').value;
+  var name2 = document.getElementById('validationTooltip02').value;
   console.log(name2);
-
-  var number = 1;
-  console.log(number);
-
-  $('#validationTooltip01').on('input', function() {
-  console.log("thishit");
-  if (isNaN(name1) && name1 > 0) {
-    $("#valid-tooltip").text("Looks Good!");
-  } else if (name1 && name1 > 0) {
-    $("#valid-tooltip").text("Write your name!");
+  
+  if (isNaN(name1) && name1.length > 0) {
+    $(".form-control").addClass("red");
+    //$("#valid-tooltip").text("Looks Good!");
+  } else if (name1 && name1.length > 0) {
+    $(".form-control").addClass("green");
+   // $("#valid-tooltip").text("Write your name!");
   } else {
-    $("#valid-tooltip").text("");
+    $(".form-control").removeClass("red,green");
   }
 
   // $("#submit").on("click", function (event) {
@@ -26,27 +26,8 @@ $(document).ready(function () {
   //   firstName: $("#validationTooltip01").val().trim(),
   //     role: $("#role").val().trim(),
   //     age: $("#age").val().trim(),
-  //     forcePoints: $("#force-points").val().trim()
-
+  
   // });
 
-  // $("#validationTooltip01").on("submit", function() {
-  //   $("#myform").validate({
-  //     // initialize the plugin
-  //     rules: {
-  //       field1: {
-  //         required: true,
-  //         email: true
-  //       },
-  //       field2: {
-  //         required: true,
-  //         minlength: 5
-  //       }
-  //     }
-  //   });
-  //   alert("Form has been submitted");
-  // });
-
-  //})
 });
 });
